d5e3fddc8dfcdb576d273ead530fa408d3276f1d
Fear837 <Fear837@web.de>
2015-10-01 14:24:07 +0200

    +Result: Added some helper methods like ifSuccess(), orElse(..)..

--- src/java/de/craftolution/craftolibrary/Result.java
+++ src/java/de/craftolution/craftolibrary/Result.java
@@ -7,7 +7,9 @@
  */
 package de.craftolution.craftolibrary;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.function.Consumer;
 
 import javax.annotation.Nullable;
 
@@ -21,7 +23,7 @@ import com.google.common.base.Supplier;
  * @since 30.08.2015 02:20:26
  * @see <a href="https://github.com/Craftolution">Craftolution on Github</a>
  */
-public class Result<T> implements Supplier<Optional<T>>, java.util.function.Supplier<Optional<T>> {
+public class Result<T> implements Supplier<T>, java.util.function.Supplier<T> {
 
 	@Nullable private final T object;
 	@Nullable private final Throwable exception;
@@ -36,8 +38,67 @@ public class Result<T> implements Supplier<Optional<T>>, java.util.function.Supp
 
 	/** TODO: Documentation */
 	@Override
-	public Optional<T> get() { return Optional.ofNullable(this.object); }
+	public T get() throws NoSuchElementException { 
+		return this.object; 
+	}
 
+	/** TODO: Documentation */
+	public Optional<T> getAsOptional() {
+		return Optional.ofNullable(this.object);
+	}
+
+	/** TODO: Docuemntation */
+	public boolean ifSuccess(Consumer<? super T> consumer) {
+		if (this.isSuccess() && consumer != null) {
+			consumer.accept(this.object);
+			return true;
+		}
+		return false;
+	}
+
+	/** TODO: Docuemntation */
+	public T orElse(T other) {
+		return this.isSuccess() ? this.object : other;
+	}
+
+	/** TODO: Docuemntation */
+	public T orElse(Result<T> other) {
+		return this.isSuccess() ? this.object : other.orNull();
+	}
+
+	/** TODO: Docuemntation */
+	public T orElse(Optional<T> other) {
+		return this.isSuccess() ? this.object : other.orElse(null);
+	}
+
+	/** TODO: Docuemntation */
+	public T orElse(Supplier<T> other) {
+		return this.isSuccess() ? this.object : other.get();
+	}
+
+	/** TODO: Docuemntation */
+	public <E extends Exception> T orThrow(E e) throws E {
+		if (this.isSuccess()) { return this.object; }
+		else { throw e; }
+	}
+
+	/** TODO: Documentation */
+	@Nullable public T orCall(Runnable runnable) {
+		if (this.isSuccess()) { return this.object; }
+		else { runnable.run(); return null; }
+	}
+
+	/** TODO: Documentation */
+	@Nullable public T orCall(Consumer<Result<T>> consumer) {
+		if (this.isSuccess()) { return this.object; }
+		else { consumer.accept(this); return null; }
+	}
+
+	/** TODO: Docuemntation */
+	public T orNull() {
+		return this.isSuccess() ? this.object : null;
+	}
+	
 	/** TODO: Documentation */
 	public Optional<Throwable> getException() { return Optional.ofNullable(this.exception); }
 
@@ -53,7 +114,7 @@ public class Result<T> implements Supplier<Optional<T>>, java.util.function.Supp
 
 	/** TODO: Documentation */
 	@SuppressWarnings("unchecked")
-	public static <T> Result<T> of(final Throwable exception) {
+	public static <T> Result<T> ofException(final Throwable exception) {
 		return (Result<T>) new Result<Object>(null, exception);
 	}
 
