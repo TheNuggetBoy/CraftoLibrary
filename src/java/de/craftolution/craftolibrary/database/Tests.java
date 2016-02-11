413436c569c4caa0b5e0cb27b834c9ed92f9a561
Fear837 <Fear837@web.de>
2016-02-11 15:17:08 +0100

    +Created test class to debug some stuff

---
+++ src/java/de/craftolution/craftolibrary/database/Tests.java
@@ -0,0 +1,92 @@
+package de.craftolution.craftolibrary.database;
+
+import java.sql.SQLException;
+
+import de.craftolution.craftolibrary.database.Database.Builder.DatabaseType;
+import de.craftolution.craftolibrary.database.query.Order;
+import de.craftolution.craftolibrary.database.query.Query;
+import de.craftolution.craftolibrary.database.result.QueryResult;
+import de.craftolution.craftolibrary.database.result.Row;
+import de.craftolution.craftolibrary.database.table.CharSet;
+import de.craftolution.craftolibrary.database.table.Engine;
+import de.craftolution.craftolibrary.database.table.IndexType;
+import de.craftolution.craftolibrary.database.table.Table;
+
+/**
+ * TODO: Documentation
+ *
+ * @author Kevin
+ * @since 11.02.2016
+ */
+public class Tests {
+
+	public static void main(final String[] args) throws IllegalStateException, ClassNotFoundException, SQLException {
+
+		// Simple Insert Query
+		final Query insertQuery1 = Query.insert("cp_users_test").columns("name", "age", "gender").values("Peter", "4", "MALE").onDuplicateKey("`id` = `id`");
+		System.out.println(insertQuery1);
+
+		// Simplified Insert Query
+		final Query insertQuery2 = Query.insert("cp_users_test").insert("name", "Peter").insert("age", "4").insert("gender", "MALE");
+		System.out.println(insertQuery2);
+
+		// Select query
+		final Query selectQuery = Query.select("name", "age").from("cp_users_test").where("`gender` = 'MALE'").orderBy("age", Order.ASCENDING).limit(4);
+		System.out.println(selectQuery);
+
+		// TODO: RemoveQuery
+
+		// TODO: UpdateQuery
+
+		// Raw query
+		final Query rawQuery = Query.of("SELECT * FROM `cp_users_test`");
+		System.out.println(rawQuery);
+
+		// Table creation
+		final Table myTable = Table.builder("cp_users_test")
+				.addPrimaryColumn(8)
+				.addString("name", c -> c.comment("The users name").index(IndexType.UNIQUE))
+				.addInt("age", c -> c.length(2).unsigned())
+				.addEnum("gender", c -> c.values("MALE", "FEMALE").standard("MALE"))
+				.addCreatedAt()
+				.engine(Engine.InnoDB)
+				.charSet(CharSet.UTF8)
+				.index("myIndex", IndexType.INDEX, "age", "gender")
+				.comment("This is a test table.")
+				.build();
+
+		// Database creation
+		final Database db = Database.builder()
+				.databaseName("craftodb")
+				.hostname("localhost")
+				.username("myuser")
+				.password("12345")
+				.port("4837")
+				.type(DatabaseType.MYSQL)
+				.connect();
+
+		// Some database methods
+		db.createTable(myTable);
+
+		// Queries
+		final QueryResult result = db.execute(Query.select("*").from("cp_users_test").limit(4, 8));
+
+		if (result.wasSuccess()) {
+			System.out.println("Affected rows: " + result.getAffectedRows());
+
+			for (final Row row : result.getRows()) {
+				System.out.println("Reading next row:");
+
+				for (final String column : result.getColumns()) {
+					System.out.println(column + ": " + row.getString(column));
+				}
+			}
+		}
+		else {
+			final Exception exception = result.getException().get();
+			exception.printStackTrace();
+		}
+
+	}
+
+}
