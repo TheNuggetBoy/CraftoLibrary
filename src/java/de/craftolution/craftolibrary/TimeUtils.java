4dbf703ce8440bdd07acaea71e75e575c7119bba
Fear837 <Fear837@web.de>
2016-04-09 23:32:16 +0200

    *TimeUtils: Revamped class for the new java8 time api

--- src/java/de/craftolution/craftolibrary/TimeUtils.java
+++ src/java/de/craftolution/craftolibrary/TimeUtils.java
@@ -9,9 +9,12 @@ package de.craftolution.craftolibrary;
 
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Utility class for formatting strings to a specific time.
@@ -27,106 +30,57 @@ public class TimeUtils {
 
 	private TimeUtils() { }
 
-	/** A dateformatter to format the given strings. */
-	private static SimpleDateFormat df = new SimpleDateFormat("dd.MM.yyyy HH:mm", Locale.GERMANY);
+	private static String currentPattern = "dd.MM.yyyy HH:mm";
+	private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern(TimeUtils.currentPattern).withZone(ZoneId.systemDefault());
 
-	/**
-	 * @return Returns the current time in the following format: <blockquote>"dd.MM.yyyy HH:mm"</blockquote>
-	 * Click <a href="http://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html">here</a> to read all format possibilites. <br> <br>
-	 */
-	public static String getCurrentTime() {
-		return TimeUtils.getCurrentTime("dd.MM.yyyy HH:mm");
+	/** TODO: Documentation */
+	public static Instant toInstant(final Timestamp timestamp) { return Instant.ofEpochMilli(timestamp.getTime()); }
+
+	/** TODO: Documentation */
+	public static Timestamp toTimestamp(final Instant instant) { return new Timestamp(instant.toEpochMilli()); }
+
+	/** TODO: Documentation */
+	public static Timestamp currentTimestamp() { return new Timestamp(System.currentTimeMillis()); }
+
+	/** TODO: Documentation */
+	public static String parseCurrentTime() { return TimeUtils.parseCurrentTime("dd.MM.yyyy HH:mm"); }
+
+	/** TODO: Documentation */
+	public static String parseCurrentTime(final String format) {
+		TimeUtils.checkFormatter(Check.notNull(format, "The format cannot be null!"));
+		synchronized (TimeUtils.formatter) { return TimeUtils.formatter.format(Instant.now()); }
 	}
 
-	/**
-	 * Checks the current time and returns it in the specified format.
-	 * @param format - The format in which the time should be returned.
-	 * @return Returns the current time in the given format.
-	 */
-	public static String getCurrentTime(final String format) {
-		Check.nonNulls("The format must not be null.", format);
-		TimeUtils.checkFormat(format);
-		synchronized (TimeUtils.df) { return TimeUtils.df.format(new Date(System.currentTimeMillis())); }
+	/** TODO: Documentation */
+	public static String parseTime(final Instant instant) {
+		synchronized (TimeUtils.formatter) { return TimeUtils.formatter.format(Check.notNull(instant, "The instant cannot be null!")); }
 	}
 
-	/**
-	 * Parses the given timestamp to a string in the following format: <blockquote>"dd.MM.yyyy HH:mm"</blockquote>
-	 * Click <a href="http://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html">here</a> to read all formatting possibilites. <br> <br>
-	 * @param time - The timestamp to parse.
-	 * @return Returns the string version of the given timestamp.
-	 */
-	public static String getTime(final Timestamp time) {
-		Check.notNull(time, "The timestamp must not be null.");
-		return TimeUtils.getTime(time.getTime(), "dd.MM.yyyy HH:mm");
+	/** TODO: Documentation */
+	public static String parseTime(final Timestamp timestamp) {
+		synchronized (TimeUtils.formatter) { return TimeUtils.formatter.format(TimeUtils.toInstant( Check.notNull(timestamp, "The timestamp cannot be null!") )); }
 	}
 
-	/**
-	 * Parses the given time (in milliseconds) to a {@link String} in the following format: <blockquote>"dd.MM.yyyy HH:mm"</blockquote>
-	 * Click <a href="http://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html">here</a> to read all formatting possibilites. <br> <br>
-	 * @param time - The time to parse.
-	 * @return Returns a string version of the given time.
-	 */
-	public static String getTime(final long time) {
-		return TimeUtils.getTime(time, "dd.MM.yyyy HH:mm");
+	/** TODO: Documentation */
+	public static String parseTime(final Instant instant, final String format) {
+		TimeUtils.checkFormatter(Check.notNull(format, "The format cannot be null!"));
+		synchronized (TimeUtils.formatter) { return TimeUtils.formatter.format( Check.notNull(instant, "The instant cannot be null!") ); }
 	}
 
-	/**
-	 * Parses the given timestamp into a string with the specified format.
-	 * Click <a href="http://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html">here</a> to read all formatting possibilites. <br> <br>
-	 * @param time - The timestamp to parse.
-	 * @param format - The format.
-	 * @return Returns a string version of the given timestamp in the specified format.
-	 */
-	public static String getTime(final Timestamp time, final String format) {
-		Check.nonNulls("The timestamp/format must not be null.", time, format);
-		return TimeUtils.getTime(time.getTime(), format);
+	/** TODO: Documentation */
+	public static String parseTime(final Timestamp timestamp, final String format) {
+		TimeUtils.checkFormatter(Check.notNull(format, "The format cannot be null!"));
+		synchronized (TimeUtils.formatter) { return TimeUtils.formatter.format(TimeUtils.toInstant( Check.notNull(timestamp, "The timestamp cannot be null!") )); }
 	}
 
-	/**
-	 * Parses the given time (in milliseconds!) into a string with the specified format.
-	 * Click <a href="http://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html">here</a> to read all format possibilites. <br> <br>
-	 * @param time - The time to parse.
-	 * @param format - The format.
-	 * @return Returns a string version of the given time in the specified format.
-	 */
-	public static String getTime(final long time, final String format) {
-		Check.nonNulls("The format must not be null.", format);
-		TimeUtils.checkFormat(format);
-		synchronized (TimeUtils.df) { return TimeUtils.df.format(new Date(time)); }
-	}
+	/** TODO: Documentation */
+	public static DayOfWeek getCurrentWeekday() { return LocalDate.now().getDayOfWeek(); }
 
-	/**
-	 * Returns the current time in milliseconds.
-	 * @return The difference, measured in milliseconds, between the current time and midnight, January 1, 1970 UTC.
-	 */
-	public static long getCurrentMillis() {
-		return System.currentTimeMillis();
-	}
-
-	/**
-	 * Checks the current weekday and returns it as a string translated by the specified language.
-	 * @param lang - The language in which the weekday should be returned.
-	 * @return Returns the current weekday.
-	 */
-	public static String getWeekday() {
-		switch (Calendar.getInstance().get(Calendar.DAY_OF_WEEK)) {
-			case 1: return "SUNDAY";
-			case 2: return "MONDAY";
-			case 3: return "TUESDAY";
-			case 4: return "WEDNESDAY";
-			case 5: return "THURSDAY";
-			case 6: return "FRIDAY";
-			case 7: return "SATURDAY";
-			default: return "$ERROR$";
+	private static void checkFormatter(final String format) {
+		if (!TimeUtils.currentPattern.equals(format)) {
+			TimeUtils.currentPattern = format;
+			TimeUtils.formatter = DateTimeFormatter.ofPattern(TimeUtils.currentPattern).withZone(ZoneId.systemDefault());
 		}
 	}
 
-	private static void checkFormat(final String format) {
-		Check.nonNulls("The format must not be null", format);
-		synchronized (TimeUtils.df) {
-			if (!TimeUtils.df.toPattern().equalsIgnoreCase(format)) {
-				TimeUtils.df = new SimpleDateFormat(format, Locale.GERMANY);
-			}
-		}
-	}
 }
