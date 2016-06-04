07a0eedb7531bbae9463a5ac3fb036e363a2154d
Fear837 <Fear837@web.de>
2016-06-04 14:55:56 +0200

    +RepeatingRunnable: Improved RepeatingRunnable
    + Consumer support
    + maxExecutions support
    + runOnce()

--- src/java/de/craftolution/craftolibrary/RepeatingRunnable.java
+++ src/java/de/craftolution/craftolibrary/RepeatingRunnable.java
@@ -4,6 +4,9 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+
+import javax.annotation.Nullable;
 
 /**
  * TODO: Documentation
@@ -17,19 +20,44 @@ public class RepeatingRunnable {
 
 	private final Duration interval;
 	private final Runnable runnable;
+	private final Consumer<RepeatingRunnable> consumer;
+	private final Runnable realRunnable = this::run;
 
 	private Runnable cancelListener;
 
-	RepeatingRunnable(final Duration interval, final Duration initialDelay, final Runnable runnable) {
+	private final int maxExecutions;
+	private int executions;
+
+	RepeatingRunnable(final Duration interval, final Duration initialDelay, final Runnable runnable, final int maxExecutions) {
 		this.interval = interval;
 		this.runnable = runnable;
-		RepeatingRunnable.executor.scheduleWithFixedDelay(runnable, initialDelay.toNanos(), interval.toNanos(), TimeUnit.NANOSECONDS);
+		this.consumer = null;
+		this.maxExecutions = maxExecutions;
+		RepeatingRunnable.executor.scheduleWithFixedDelay(this.realRunnable, initialDelay.toNanos(), interval.toNanos(), TimeUnit.NANOSECONDS);
+	}
+
+	RepeatingRunnable(final Duration interval, final Duration initialDelay, final Consumer<RepeatingRunnable> consumer, final int maxExecutions) {
+		this.interval = interval;
+		this.runnable = null;
+		this.consumer = consumer;
+		this.maxExecutions = maxExecutions;
+		RepeatingRunnable.executor.scheduleWithFixedDelay(this.realRunnable, initialDelay.toNanos(), interval.toNanos(), TimeUnit.NANOSECONDS);
+	}
+
+	/** TODO: Documentation */
+	public void run() {
+		if (this.maxExecutions == -1 || this.executions < this.maxExecutions) {
+			this.executions++;
+			if (this.runnable != null) { this.runnable.run(); }
+			else if (this.consumer != null) { this.consumer.accept(this); }
+		}
 	}
 
 	/** TODO: Documentation */
 	public boolean cancel() {
-		final boolean result = RepeatingRunnable.executor.remove(this.runnable);
+		final boolean result = RepeatingRunnable.executor.remove(this.realRunnable);
 		if (this.cancelListener != null) { this.cancelListener.run(); }
+		System.out.println("cancelled");
 		return result;
 	}
 
@@ -46,16 +74,43 @@ public class RepeatingRunnable {
 	public Duration getInterval() { return this.interval; }
 
 	/** TODO: Documentation */
-	public Runnable getRunnable() { return this.runnable; }
+	public int getExecutionCount() { return this.executions; }
 
 	/** TODO: Documentation */
-	public static RepeatingRunnable run(final Duration interval, final Runnable runnable) {
-		return new RepeatingRunnable(interval, Duration.ZERO, runnable);
+	public int getMaxExecutions() { return this.maxExecutions; }
+
+	/** TODO: Documentation */
+	@Nullable public Runnable getRunnable() { return this.runnable; }
+
+	/** TODO: Documentation */
+	@Nullable public Consumer<RepeatingRunnable> getConsumer() { return this.consumer; }
+
+	/** TODO: Documentation */
+	public static RepeatingRunnable runOnce(final Duration delay, final Runnable runnable) { return new RepeatingRunnable(Duration.ofMillis(1), delay, runnable, 1); }
+
+	/** TODO: Documentation */
+	public static RepeatingRunnable run(final Duration interval, final Runnable runnable) { return new RepeatingRunnable(interval, Duration.ZERO, runnable, -1); }
+
+	/** TODO: Documentation */
+	public static RepeatingRunnable run(final Duration interval, final Duration initialDelay, final Runnable runnable) { return new RepeatingRunnable(interval, initialDelay, runnable, -1); }
+
+	/** TODO: Documentation */
+	public static RepeatingRunnable run(final Duration interval, final Duration initialDelay, final int maxExecutions, final Runnable runnable) {
+		return new RepeatingRunnable(interval, initialDelay, runnable, maxExecutions);
 	}
 
 	/** TODO: Documentation */
-	public static RepeatingRunnable run(final Duration interval, final Duration initialDelay, final Runnable runnable) {
-		return new RepeatingRunnable(interval, initialDelay, runnable);
+	public static RepeatingRunnable runOnce(final Duration delay, final Consumer<RepeatingRunnable> consumer) { return new RepeatingRunnable(Duration.ofMillis(1), delay, consumer, 1); }
+
+	/** TODO: Documentation */
+	public static RepeatingRunnable run(final Duration interval, final Consumer<RepeatingRunnable> consumer) { return new RepeatingRunnable(interval, Duration.ZERO, consumer, -1); }
+
+	/** TODO: Documentation */
+	public static RepeatingRunnable run(final Duration interval, final Duration initialDelay, final Consumer<RepeatingRunnable> consumer) { return new RepeatingRunnable(interval, initialDelay, consumer, -1); }
+
+	/** TODO: Documentation */
+	public static RepeatingRunnable run(final Duration interval, final Duration initialDelay, final int maxExecutions, final Consumer<RepeatingRunnable> consumer) {
+		return new RepeatingRunnable(interval, initialDelay, consumer, maxExecutions);
 	}
 
 }
