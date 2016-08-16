db13adbf4329a6feebf3879d3f390d73e69f3088
Fear837 <Fear837@web.de>
2016-08-16 23:57:35 +0200

    +Row: Added getters with default values

--- src/java/de/craftolution/craftolibrary/database/Row.java
+++ src/java/de/craftolution/craftolibrary/database/Row.java
@@ -16,6 +16,7 @@ import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Set;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import com.google.common.collect.Maps;
@@ -79,6 +80,19 @@ public class Row implements Serializable {
 	}
 
 	/**
+	 * Retrieves the value of the designated column in the current row as a {@code String} in the Java programming language.
+	 * If the value is not present, the given {@code defaultValue} is returned.
+	 * @param columnLabel - The label for the column specified with the SQL AS clause. If the SQL AS clause was not specified, then the label is the name of the column.
+	 * @return Returns the column value; if the value is {@code SQL NULL}, the given {@code defaultValue} is returned.
+	 * @throws IllegalArgumentException If the specified columnLabel was either {@code null} or empty.
+	 * @throws NoSuchElementException If no column exists that corresponds to the specified name/label.
+	 */
+	@Nonnull public String getString(final String columnLabel, String defaultValue) throws IllegalArgumentException, NoSuchElementException {
+		final Object value = this.pull(columnLabel);
+		return value != null ? value.toString() : defaultValue;
+	}
+
+	/**
 	 * Retrieves the value of the designated column in the current row as a {@code Integer} in the Java programming language.
 	 * @param columnLabel - The label for the column specified with the SQL AS clause. If the SQL AS clause was not specified, then the label is the name of the column.
 	 * @return Returns the column value; if the value is {@code SQL NULL}, the value returned is null.
@@ -94,6 +108,22 @@ public class Row implements Serializable {
 	}
 
 	/**
+	 * Retrieves the value of the designated column in the current row as a {@code Integer} in the Java programming language.
+	 * If the value is not present, the given {@code defaultValue} is returned.
+	 * @param columnLabel - The label for the column specified with the SQL AS clause. If the SQL AS clause was not specified, then the label is the name of the column.
+	 * @return Returns the column value; if the value is {@code SQL NULL}, the value returned is null.
+	 * @throws IllegalArgumentException If the specified columnLabel was either {@code null} or empty.
+	 * @throws NoSuchElementException If no column exists that corresponds to the specified name/label.
+	 * @throws NumberFormatException If the value of the column couldn't be parsed to an integer.
+	 */
+	@Nonnull public Integer getInt(final String columnLabel, Integer defaultValue) throws IllegalArgumentException, NoSuchElementException {
+		final Object value = this.pull(columnLabel);
+		if (value == null) { return defaultValue; }
+		if (value instanceof Integer) { return (Integer) value; }
+		else { return Integer.parseInt(value.toString()); }
+	}
+
+	/**
 	 * Retrieves the value of the designated column in the current row as a {@code Double} in the Java programming language.
 	 * @param columnLabel - The label for the column specified with the SQL AS clause. If the SQL AS clause was not specified, then the label is the name of the column.
 	 * @return Returns the column value; if the value is {@code SQL NULL}, the value returned is null.
@@ -104,6 +134,22 @@ public class Row implements Serializable {
 	@Nullable public Double getDouble(final String columnLabel) throws IllegalArgumentException, NoSuchElementException, NumberFormatException {
 		final Object value = this.pull(columnLabel);
 		if (value == null) { return null; }
+		if (value instanceof Double) { return (Double) value; }
+		else { return Double.parseDouble(value.toString()); }
+	}
+	
+	/**
+	 * Retrieves the value of the designated column in the current row as a {@code Double} in the Java programming language.
+	 * If the value is not present, the given {@code defaultValue} is returned.
+	 * @param columnLabel - The label for the column specified with the SQL AS clause. If the SQL AS clause was not specified, then the label is the name of the column.
+	 * @return Returns the column value; if the value is {@code SQL NULL}, the value returned is null.
+	 * @throws IllegalArgumentException If the specified columnLabel was either {@code null} or empty.
+	 * @throws NoSuchElementException If no column exists that corresponds to the specified name/label.
+	 * @throws NumberFormatException If the value of the column couldn't be parsed to an double.
+	 */
+	@Nonnull public Double getDouble(final String columnLabel, Double defaultValue) throws IllegalArgumentException, NoSuchElementException, NumberFormatException {
+		final Object value = this.pull(columnLabel);
+		if (value == null) { return defaultValue; }
 		if (value instanceof Double) { return (Double) value; }
 		else { return Double.parseDouble(value.toString()); }
 	}
@@ -124,6 +170,22 @@ public class Row implements Serializable {
 	}
 
 	/**
+	 * Retrieves the value of the designated column in the current row as a {@code Float} in the Java programming language.
+	 * If the value is not present, the given {@code defaultValue} is returned.
+	 * @param columnLabel - The label for the column specified with the SQL AS clause. If the SQL AS clause was not specified, then the label is the name of the column.
+	 * @return Returns the column value; if the value is {@code SQL NULL}, the value returned is null.
+	 * @throws IllegalArgumentException If the specified columnLabel was either {@code null} or empty.
+	 * @throws NoSuchElementException If no column exists that corresponds to the specified name/label.
+	 * @throws NumberFormatException If the value of the column couldn't be parsed to an double.
+	 */
+	@Nonnull public Float getFloat(final String columnLabel, Float defaultValue) throws IllegalArgumentException, NoSuchElementException, NumberFormatException {
+		final Object value = this.pull(columnLabel);
+		if (value == null) { return defaultValue; }
+		if (value instanceof Float) { return (Float) value; }
+		else { return Float.parseFloat(value.toString()); }
+	}
+
+	/**
 	 * Retrieves the value of the designated column in the current row as a {@code Boolean} in the Java programming language.
 	 * @param columnLabel - The label for the column specified with the SQL AS clause. If the SQL AS clause was not specified, then the label is the name of the column.
 	 * @return Returns the column value; if the value is {@code SQL NULL}, the value returned is null.
@@ -134,6 +196,25 @@ public class Row implements Serializable {
 	@Nullable public Boolean getBoolean(final String columnLabel) throws IllegalArgumentException, NoSuchElementException {
 		final Object value = this.pull(columnLabel);
 		if (value == null) { return null; }
+		if (value instanceof Boolean) { return (Boolean) value; }
+		else {
+			final String valueAsString = value.toString().toLowerCase();
+			return valueAsString.equals("1") || valueAsString.equals("true") || valueAsString.equals("yes");
+		}
+	}
+
+	/**
+	 * Retrieves the value of the designated column in the current row as a {@code Boolean} in the Java programming language.
+	 * If the value is not present, the given {@code defaultValue} is returned.
+	 * @param columnLabel - The label for the column specified with the SQL AS clause. If the SQL AS clause was not specified, then the label is the name of the column.
+	 * @return Returns the column value; if the value is {@code SQL NULL}, the value returned is null.
+	 * @throws IllegalArgumentException If the specified columnLabel was either {@code null} or empty.
+	 * @throws NoSuchElementException If no column exists that corresponds to the specified name/label.
+	 * @throws NumberFormatException If the value of the column couldn't be parsed to an double.
+	 */
+	@Nonnull public Boolean getBoolean(final String columnLabel, Boolean defaultValue) throws IllegalArgumentException, NoSuchElementException {
+		final Object value = this.pull(columnLabel);
+		if (value == null) { return defaultValue; }
 		if (value instanceof Boolean) { return (Boolean) value; }
 		else {
 			final String valueAsString = value.toString().toLowerCase();
@@ -157,10 +238,33 @@ public class Row implements Serializable {
 		else { return Timestamp.valueOf(value.toString()); }
 	}
 
+	/**
+	 * Retrieves the value of the designated column in the current row as a {@code Timestamp} in the Java programming language.
+	 * If the value is not present, the given {@code defaultValue} is returned.
+	 * @param columnLabel - The label for the column specified with the SQL AS clause. If the SQL AS clause was not specified, then the label is the name of the column.
+	 * @return Returns the column value; if the value is {@code SQL NULL}, the value returned is null.
+	 * @throws IllegalArgumentException If the specified columnLabel was either {@code null} or empty.
+	 * @throws NoSuchElementException If no column exists that corresponds to the specified name/label.
+	 */
+	@Nonnull public Timestamp getTimestamp(final String columnLabel, Timestamp defaultValue) throws IllegalArgumentException, NoSuchElementException {
+		final Object value = this.pull(columnLabel);
+		if (value == null) { return defaultValue; }
+		if (value instanceof Timestamp) { return (Timestamp) value; }
+		else { return Timestamp.valueOf(value.toString()); }
+	}
+
 	/** TODO: Documentation */
 	@Nullable public Instant getInstant(final String columnLabel) throws IllegalArgumentException, NoSuchElementException {
 		final Object value = this.pull(columnLabel);
 		if (value == null) { return null; }
+		if (value instanceof Timestamp) { return TimeUtils.toInstant((Timestamp) value); }
+		else { return TimeUtils.toInstant(Timestamp.valueOf(value.toString())); }
+	}
+
+	/** TODO: Documentation */
+	@Nonnull public Instant getInstant(final String columnLabel, Instant defaultValue) throws IllegalArgumentException, NoSuchElementException {
+		final Object value = this.pull(columnLabel);
+		if (value == null) { return defaultValue; }
 		if (value instanceof Timestamp) { return TimeUtils.toInstant((Timestamp) value); }
 		else { return TimeUtils.toInstant(Timestamp.valueOf(value.toString())); }
 	}
